82b74d6a397a79cdeb90e436455a6373df5ca897
allosaurus.scientificus <a.scientificus@icloud.com>
2020-03-29 09:53:46 -0600

    added cross join and first swing at imputing missing values

--- r/cdot_daily.R
+++ r/cdot_daily.R
@@ -1,6 +1,8 @@
 library(tidyverse)
+library(lubridate)
+library(tsibble)
 
-sql_cdot <- read_file("~/Documents/repos/coced2/sql/cdot_daily_county.sql")
+sql_cdot <- read_file("~/Documents/repos/coceasci/sql/cdot_daily_county.sql")
 
 conn <- fouu::connect_coce()
 
@@ -12,3 +14,72 @@ rm(conn)
 
 glimpse(d_cdot)
 
+
+d_d1 <- d_cdot %>% 
+  as_tibble() %>% 
+  mutate(weekend = wday(date) %in% c(1, 7),
+         quarter = floor_date(date, 'quarter')) %>% 
+  select(county, quarter, date, weekend, d1_total)
+  
+d_medians <- d_d1 %>% 
+  group_by(county, quarter, weekend) %>% 
+  summarize(d1_median = as.integer(floor(median(d1_total, na.rm = TRUE))))
+
+
+d_d1_adj <- d_d1 %>% 
+  left_join(d_medians, 
+            by = c("county" = "county",
+                   "quarter" = "quarter", 
+                   "weekend" = "weekend")) %>% 
+  mutate(d1_diff = d1_total - d1_median,
+         d1_adj = coalesce(d1_total, d1_median))
+
+
+d_d1_adj %>% 
+  filter(county == "Adams") %>% 
+  print(n = 300)
+
+
+d_d1_adj %>% 
+  filter(date >= "2019-07-01") %>% 
+  group_by(date) %>% 
+  summarize(d1 = sum(d1_total, na.rm = TRUE),
+            d1_adj = sum(d1_adj, na.rm = TRUE)) %>% 
+  gather(key = measure, value = d1_totals, -date) %>% 
+  ggplot(aes(x = date,
+             y = d1_totals,
+             linetype = fct_rev(measure))) +
+  geom_line() +
+  geom_vline(xintercept = ymd("2020-03-16"),
+             colour = "steelblue3",
+             linetype = 3) +
+  annotate("text",
+           x = ymd("2020-03-16"),
+           y = 10^4,
+           colour = "steelblue3",
+           label = paste0("3/16 bars, restaurants"),
+           hjust = "right") +
+  geom_vline(xintercept = ymd("2020-03-26"),
+             colour = "steelblue4",
+             linetype = 4) +
+  annotate("text",
+           x = ymd("2020-03-26"),
+           y = 4 * 10^5,
+           colour = "steelblue4",
+           label = paste0("3/26 stay in place"),
+           hjust = "right") +
+  scale_x_date(date_labels = "%Y-%m-%d") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(x = "measure date",
+       y = "volume totals",
+       linetype = "raw or adjusted",
+       colour = "raw or adjusted",
+       title = "unreviewed, sum of raw (d1) and median imputed (d1_adj) daily volume from cdot",
+       subtitle = "date aggregated by device to county for all counties") +
+  theme_classic() +
+  theme(legend.position = "bottom")
+
+
+
+ts_d1_adj <- d_d1_adj %>% 
+  as_tsibble(key = "county", index = "date")
--- sql/cdot_daily_county.sql
+++ sql/cdot_daily_county.sql
@@ -1,4 +1,13 @@
-with county_devices as (
+--  gaps in record so need all dates
+with date_range as (
+
+  select ds::date as measure_date
+  from generate_series('2019-05-01'::timestamp, (current_date - interval '1 day')::date, '1 day'::interval) as ds
+
+),
+
+--  device description is only way match up with county
+county_devices as (
 
   select device_desc
     , county
@@ -10,6 +19,19 @@ with county_devices as (
  
 ),
 
+--  need all combination of counties and dates
+counties_x_dates as (
+
+  select distinct county_devices.county as county
+    , date_range.measure_date
+  from county_devices
+    cross join date_range
+  where county_devices.county is not null
+  order by 1, 2
+
+),
+
+--  aggregate to county days as counties can have > device, location_id
 county_days as (
  
   select cd.county
@@ -23,30 +45,48 @@ county_days as (
   group by 1, 2
   order by 1, 2
   
-  )
-  
-select cd.county
-  , cd.measure_date
+)
+
+--  create 7 day interval lags for each mesaure
+select cxd.county as county
+  , cxd.measure_date as date
   , d1_total
---  rinse, repeat for each measure, show_lagX_diff just to highlight issue case when is mitigating
-  , (cd.measure_date - lag(cd.measure_date, 7) over (partition by cd.county order by cd.measure_date asc))::integer as show_lag7_diff
-  , case when (cd.measure_date - lag(cd.measure_date, 7) over (partition by cd.county order by cd.measure_date asc))::integer = 7::integer
-         then lag(cd.d1_total, 7) over (partition by cd.county order by cd.measure_date asc)
+--  totals
+  , lag(cd.d1_total, 7) over (partition by cxd.county order by cxd.measure_date asc) as d7_total
+  , lag(cd.d1_total, 14) over (partition by cxd.county order by cxd.measure_date asc) as d14_total
+  , lag(cd.d1_total, 28) over (partition by cxd.county order by cxd.measure_date asc) as d28_total
+--  cars
+  , lag(cd.d1_car, 7) over (partition by cxd.county order by cxd.measure_date asc) as d7_car
+  , lag(cd.d1_car, 14) over (partition by cxd.county order by cxd.measure_date asc) as d14_car
+  , lag(cd.d1_car, 28) over (partition by cxd.county order by cxd.measure_date asc) as d28_car
+--  trucks
+  , lag(cd.d1_truck, 7) over (partition by cxd.county order by cxd.measure_date asc) as d7_truck
+  , lag(cd.d1_truck, 14) over (partition by cxd.county order by cxd.measure_date asc) as d14_truck
+  , lag(cd.d1_truck, 28) over (partition by cxd.county order by cxd.measure_date asc) as d28_truck
+--  unbinned
+  , lag(cd.d1_unbinned, 7) over (partition by cxd.county order by cxd.measure_date asc) as d7_unbinned
+  , lag(cd.d1_unbinned, 14) over (partition by cxd.county order by cxd.measure_date asc) as d14_unbinned
+  , lag(cd.d1_unbinned, 28) over (partition by cxd.county order by cxd.measure_date asc) as d28_unbinned
+/*  old mitigation
+  , (cxd.measure_date - lag(cxd.measure_date, 7) over (partition by cxd.county order by cxd.measure_date asc))::integer as show_lag7_diff
+  , case when (cxd.measure_date - lag(cxd.measure_date, 7) over (partition by cxd.county order by cxd.measure_date asc))::integer = 7::integer
+         then lag(cd.d1_total, 7) over (partition by cxd.county order by cxd.measure_date asc)
          else null
     end as d7_total_sql
-  , (cd.measure_date - lag(cd.measure_date, 14) over (partition by cd.county order by cd.measure_date asc))::integer as show_lag14_diff
-  , case when (cd.measure_date - lag(cd.measure_date, 14) over (partition by cd.county order by cd.measure_date asc))::integer = 14::integer
-         then lag(cd.d1_total, 14) over (partition by cd.county order by cd.measure_date asc)
-         else null
-    end as d14_total_sql
-  , (cd.measure_date - lag(cd.measure_date, 28) over (partition by cd.county order by cd.measure_date asc))::integer as show_lag28_diff
-  , case when (cd.measure_date - lag(cd.measure_date, 28) over (partition by cd.county order by cd.measure_date asc))::integer = 28::integer
-         then lag(cd.d1_total, 28) over (partition by cd.county order by cd.measure_date asc)
-         else null
-    end as d28_total_sql
-  , (cd.measure_date - lag(cd.measure_date, 42) over (partition by cd.county order by cd.measure_date asc))::integer as show_lag42_diff   
-  , case when (cd.measure_date - lag(cd.measure_date, 42) over (partition by cd.county order by cd.measure_date asc))::integer = 42::integer
-         then lag(cd.d1_total, 42) over (partition by cd.county order by cd.measure_date asc)
-         else null
-    end as d42_total_sql
-from county_days as cd
+*/
+
+from counties_x_dates as cxd
+  left join county_days as cd on (cxd.county = cd.county and cxd.measure_date = cd.measure_date)
+--  where cxd.county = 'Adams'
+
+order by 1, 2
+
+
+/*  testing, confirming adams has no cars or trucks but things that go
+
+select * 
+  from dim.cdot_atr_daily_fact
+    inner join dim.cdot_device_inventory on dim.cdot_atr_daily_fact.device = dim.cdot_device_inventory.device_desc
+where dim.cdot_device_inventory.county = 'Adams'
+
+*/
