965fdff7cf105345fdb8d170fd9a1f938dd20002
allosaurus.scientificus <a.scientificus@icloud.com>
2020-04-02 19:09:43 -0600

    rm'd dup cols

--- sql/municipality_mobility_expanded.sql
+++ sql/municipality_mobility_expanded.sql
@@ -2,7 +2,7 @@
 with date_range as (
 
   select ds::date as date
-  from generate_series('2020-03-01'::timestamp, (current_date - interval '1 day')::date, '1 day'::interval) as ds
+  from generate_series('2020-03-01'::date, (current_date - interval '1 day')::date, '1 day'::interval) as ds
 
 ),
 
@@ -46,7 +46,14 @@ municipality_mobility as (
 
 )
 
-select *
+select cmd.*
+  , mm.state
+  , mm.fips
+  , mm.median_distance_traveled_km
+  , mm.delta_median_distance_traveled_km
+  , mm.mobility_index
+  , mm.delta_mobility_index
+  
 from counties_municipalities_dates as cmd 
   left join municipality_mobility as mm 
     on (cmd.county = mm.county and cmd.municipality = mm.municipality and cmd.date = mm.date)
